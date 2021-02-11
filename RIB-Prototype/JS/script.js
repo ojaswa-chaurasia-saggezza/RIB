85ca8e12fe399a44c8e3f68a1bb0c2a4c5f78fa2
Nayan Verma <nayan.pravesh@saggezza.com>
2021-02-11 12:08:13 +0530

    /

--- RIB-Prototype/JS/script.js
+++ RIB-Prototype/JS/script.js
@@ -1,4 +1,8 @@
-$('.btn').click(function () {
+// This file contains the JS code for all the files in the dashboard
+
+//The functions below helps in the functioning of the side menu
+
+$('nav .btn').click(function () {
   $(this).toggleClass("click");
   $('.sidebar').toggleClass("show");
 });
@@ -11,9 +15,20 @@ $('nav ul li').click(function () {
   $(this).addClass("active").siblings().children().children().removeClass("active");
 });
 
-if($(window).width()< 992)
-{
-  $('.btn').toggleClass("click");
+if ($(window).width() < 992) {
+  $('nav .btn').toggleClass("click");
   $('.sidebar').toggleClass("show");
+}
+
+// This function would return the strength of the 
+var passwordValidation = (pass) => {
+  var strength = 1;
+  var arr = [/.{8,16}/, /[a-z]+/, /[0-9]+/, /[A-Z]+/,/[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+/];
+  jQuery.map(arr, function (regexp) {
+    if (pass.match(regexp))
+      strength++;
+  });
+  console.log(strength);
+  return strength
 }
 
