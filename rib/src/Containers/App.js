fa6f78a249dbef3c943f891f22b6fec15f3de72c
Ojaswa Chaurasia <ojaswa.chaurasia@SGZ.com>
2021-02-18 15:40:03 +0530

    added Login SignUp , Reset Password

--- rib/src/Containers/App.js
+++ rib/src/Containers/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 
 import './Dashboard/Dashboard';
+import Login from './Login Signup/Login';
+import SignUp from './Login Signup/SignUp';
 import Dashboard from './Dashboard/Dashboard';
+import ResetPassword from '../Containers/Login Signup/ResetPassword';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Login}/>
+          <Route path='/SignUp' component={SignUp}/>
+          <Route path='/ResetPassword' component={ResetPassword}/>
+          <Route path='/CASA' component={Dashboard}/>
+          
+        </Switch>
+      </Router>
 
-      <Dashboard />
-      
     </div>
   );
 }
---
+++ rib/src/Containers/Login Signup/ResetPassword.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Dialog from '../../Elements/Dialog';
+import {Link as RouteLink}  from "react-router-dom";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignUp() {
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Reset Password
+        </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>  
+                        <Grid item xs={12}>
+                        <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="current-password"
+                                label="Current Password"
+                                type="current-password"
+                                id="current-password"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="confirm-password"
+                                label="Confirm Password"
+                                type="confirm-password"
+                                id="confirm-password"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        component={RouteLink} 
+                        to={'/Login'}
+                        className={classes.submit}
+                    >
+                        Change Password
+          </Button>
+                </form>
+            </div>
+            <Box mt={5}>
+                <Copyright />
+            </Box>
+        </Container>
+    );
+}
