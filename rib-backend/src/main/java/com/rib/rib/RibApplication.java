a61f27aa38a02927be526847720861c4af4f0ed5
Shanti Mukati <shanti.mukati@SGZ.com>
2021-03-02 15:55:26 +0530

    the sprint boot files added

---
+++ rib-backend/src/main/java/com/rib/rib/RibApplication.java
@@ -0,0 +1,13 @@
+package com.rib.rib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibApplication.class, args);
+	}
+
+}
