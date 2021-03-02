b543e924ecf9f9818e811412cc38683a04c6046a
Shanti Mukati <shanti.mukati@SGZ.com>
2021-03-02 18:15:14 +0530

    Created our 1st API at '/api/v1/Customer'

---
+++ rib-backend/src/main/java/com/rib/rib/controller/CustomerController.java
@@ -0,0 +1,24 @@
+
+package com.rib.rib.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rib.rib.model.Customer;
+import com.rib.rib.repository.CustomerRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class CustomerController {
+@Autowired	
+private CustomerRepository customerRepository;
+@GetMapping("/Customer")
+public List<Customer>getallCustomers()
+{
+	return customerRepository.findAll();
+}
+
+}
