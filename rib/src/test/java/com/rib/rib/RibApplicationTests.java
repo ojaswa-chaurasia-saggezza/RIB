5305ef8c819639ac880fe8d7c09f604773180159
Shanti Mukati <shanti.mukati@SGZ.com>
2021-03-02 15:10:28 +0530

    Springboot added

---
+++ rib/src/test/java/com/rib/rib/RibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rib.rib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
