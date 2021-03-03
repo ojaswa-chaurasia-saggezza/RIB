5f1ca0f19c2c1f6ffc5209d4c2eab12fdccb7ba9
Nayan Verma <nayan.pravesh@saggezza.com>
2021-03-03 15:43:36 +0530

    Account table added

---
+++ rib-backend/src/main/java/com/rib/rib/model/Account.java
@@ -0,0 +1,100 @@
+package com.rib.rib.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class Account {
+	@Id
+	private Long accountNumber;
+	@Column
+	private Long balance;
+	@Column
+	private String type;
+	@Column
+	private Long outStandingBalance;
+	@Column
+	private Long outStandingDueDate;
+	@Column
+	private String Segment;
+	@Column
+	private String IFSC;
+	
+	public Account() {
+		
+	}
+
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Long balance) {
+		this.balance = balance;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getOutStandingBalance() {
+		return outStandingBalance;
+	}
+
+	public void setOutStandingBalance(Long outStandingBalance) {
+		this.outStandingBalance = outStandingBalance;
+	}
+
+	public Long getOutStandingDueDate() {
+		return outStandingDueDate;
+	}
+
+	public void setOutStandingDueDate(Long outStandingDueDate) {
+		this.outStandingDueDate = outStandingDueDate;
+	}
+
+	public String getSegment() {
+		return Segment;
+	}
+
+	public void setSegment(String segment) {
+		Segment = segment;
+	}
+
+	public String getIFSC() {
+		return IFSC;
+	}
+
+	public void setIFSC(String iFSC) {
+		IFSC = iFSC;
+	}
+
+	public Account(Long accountNumber, Long balance, String type, Long outStandingBalance, Long outStandingDueDate,
+			String segment, String iFSC) {
+		super();
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.type = type;
+		this.outStandingBalance = outStandingBalance;
+		this.outStandingDueDate = outStandingDueDate;
+		Segment = segment;
+		IFSC = iFSC;
+	}
+	
+	
+	
+}
