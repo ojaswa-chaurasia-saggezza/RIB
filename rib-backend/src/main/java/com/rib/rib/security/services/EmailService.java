792c13f28804c116fcbab47a869930a8d78cf556
Ojaswa Chaurasia <ojaswa.chaurasia@SGZ.com>
2021-03-09 17:59:23 +0530

    Formatted Email for OTP

--- rib-backend/src/main/java/com/rib/rib/security/services/EmailService.java
+++ rib-backend/src/main/java/com/rib/rib/security/services/EmailService.java
@@ -1,4 +1,5 @@
 package com.rib.rib.security.services;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
@@ -7,21 +8,21 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class EmailService {
-	
-@Autowired
-private JavaMailSender javaMailSender;
-public void sendOtpMessage(String to, String subject, String message) throws MessagingException {
-	
-	 MimeMessage msg = javaMailSender.createMimeMessage();
 
-       MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+	@Autowired
+	private JavaMailSender javaMailSender;
 
-       helper.setTo(to);
-       helper.setSubject(subject);
-       helper.setText(message, true);
-       javaMailSender.send(msg);
-}
+	public void sendOtpMessage(String to, String subject, String message) throws MessagingException {
+
+		MimeMessage msg = javaMailSender.createMimeMessage();
+
+		MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(message, true);
+		javaMailSender.send(msg);
+	}
 }
--- rib-backend/src/main/java/com/rib/rib/security/services/OTPService.java
+++ rib-backend/src/main/java/com/rib/rib/security/services/OTPService.java
@@ -8,54 +8,46 @@ import com.google.common.cache.LoadingCache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 
-
 @Service
 public class OTPService {
-	//Assigning the expiry time of OTP to 3 minutes
+	// Assigning the expiry time of OTP to 3 minutes
 	private static final Integer EXPIRE_MINS = 3;
-	private LoadingCache<String ,Integer> otpCache;
-	
-	public OTPService()
-	{
-		super();
-		otpCache = CacheBuilder.newBuilder().
-				expireAfterWrite(EXPIRE_MINS, TimeUnit.MINUTES)
-				.build(new CacheLoader<String, Integer>()
-				{
-			  public Integer load(String Key)
-			  {
-			  return 0;
-				}
-	});
+	private LoadingCache<String, Integer> otpCache;
 
-}
+	public OTPService() {
+		super();
+		otpCache = CacheBuilder.newBuilder().expireAfterWrite(EXPIRE_MINS, TimeUnit.MINUTES)
+				.build(new CacheLoader<String, Integer>() {
+					public Integer load(String Key) {
+						return 0;
+					}
+				});
+
+	}
 
 //Generate and return OTP value
-public int generateOTP(String key)
-{
-	Random random = new Random();
-	int otp = 100000 + random.nextInt(900000);
-	otpCache.put(key, otp); 
-	return otp;
-	
-}
+	public int generateOTP(String key) {
+		Random random = new Random();
+		int otp = 100000 + random.nextInt(900000);
+		otpCache.put(key, otp);
+		return otp;
+
+	}
 
 //Fetch OTP From Cache file
-public int getOtp(String key)
-{
-	try {
-		return otpCache.get(key);
-		
-	}catch (Exception e){
-	
-		return 0;
+	public int getOtp(String key) {
+		try {
+			return otpCache.get(key);
+
+		} catch (Exception e) {
+
+			return 0;
+		}
+
 	}
-	
-}
 
 // Remove OTP
-public void clearOTP(String key)
-{
-	otpCache.invalidate(key);
-}
+	public void clearOTP(String key) {
+		otpCache.invalidate(key);
+	}
 }
